6e4cd07cb128af9b96474b43cbab9a7a37b1f2c3
gabsgc <gabriellagarccia@gmail.com>
2021-06-03 16:17:09 -0300

    first commit

---
+++ application/model/entity/tool.py
@@ -0,0 +1,47 @@
+class Tool:
+    def __init__(self, id:int, name: str, link: str, description: str, tags_list: str):
+        self._id = id
+        self._name = name
+        self._link = link
+        self._description = description
+        self._tags = tags_list
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self, value):
+        self._id = value
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
+    
+    @property
+    def link(self):
+        return self._link
+    
+    @link.setter
+    def link(self, value):
+        self._link = value
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        self._description = value
+    
+    @property
+    def tag(self):
+        return self._tags
+    
+    @tag.setter
+    def tag(self, value):
+        self._tags = value
